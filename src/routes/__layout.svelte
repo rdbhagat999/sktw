4c97b05fd483706de8bf1ec058deeef945c0144c
Ramandeep <rdbhagat999@gmail.com>
2021-05-19 16:40:00 +0530

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import appCSS from '../app.css';
+
+</script>
+
+<slot />
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    "./src/**/*.{html,css,js,ts,pcss,scss,tsx,jsx,svelte,astro}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
