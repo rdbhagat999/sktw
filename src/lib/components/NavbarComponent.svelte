0582c4537908b770f2af934d20cd6d9a92a99704
Ramandeep <rdbhagat999@gmail.com>
2021-05-20 13:22:36 +0530

    adds navabar

---
+++ src/lib/components/NavbarComponent.svelte
@@ -0,0 +1,42 @@
+<script>
+	let isOpen = false;
+	let classDefault =
+		'px-2 pt-1 pb-4 space-y-2 md:space-y-0 md:flex md:p-0 transition transform duration-700 hidden';
+	let classOpen =
+		'px-2 pt-1 pb-4 space-y-2 md:space-y-0 md:flex md:p-0 transition transform duration-700 block';
+
+</script>
+
+<header class="bg-gray-900 md:flex md:justify-between md:items-center md:px-4 md:py-3">
+	<div class="p-4 flex justify-between items-center md:p-0">
+		<div>
+			<img class="h-10" src="/svg/logo-inverted.svg" alt="logo" />
+		</div>
+		<div class="md:hidden">
+			<button
+				on:click={() => (isOpen = !isOpen)}
+				type="button"
+				class="text-gray-500 block hover:text-white focus:text-white focus:outline-none"
+			>
+				<svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+					{#if isOpen}
+						<path
+							fill-rule="evenodd"
+							d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+						/>
+					{:else}
+						<path
+							fill-rule="evenodd"
+							d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+						/>
+					{/if}
+				</svg>
+			</button>
+		</div>
+	</div>
+	<div class={isOpen ? classOpen : classDefault}>
+		<a href="#" class="block rounded px-2 py-1 text-gray-100 hover:bg-gray-800">Home</a>
+		<a href="#" class="block rounded px-2 py-1 text-gray-100 hover:bg-gray-800">About</a>
+		<a href="#" class="block rounded px-2 py-1 text-gray-100 hover:bg-gray-800">Contact</a>
+	</div>
+</header>
