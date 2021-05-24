73dfabefd35425218fe07c426cf8a365ee47ed69
Ramandeep <rdbhagat999@gmail.com>
2021-05-24 19:27:59 +0530

    adds profileCardComponent

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { popularProperties } from '$lib/data/index';
+	import ProfileCardComponent from '$lib/components/ProfileCardComponent.svelte';
 	import CardComponent from '$lib/components/CardComponent.svelte';
 
 </script>
 
 <div class="bg-gray-100 min-h-screen p-8 text-center">
+	<ProfileCardComponent />
 	<CardComponent {popularProperties} />
 </div>
